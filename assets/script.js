fc58b0c8425c5ad236efe43cc5e336ac8dbc4db5
Cindy <cindydunham4@gmail.com>
2022-05-06 22:36:21 -0400

    starting script js

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    var init = false;
+    var now = moment().format('LLLL');
+
+})
+
+// current day at the top of the calendar
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+//timeblocks and color changes
+function colorChange() {
+    
+}
