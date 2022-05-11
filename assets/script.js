1e93366c7d552c823f20cc7bb07ab51c73eee3a9
Cindy <cindydunham4@gmail.com>
2022-05-11 05:05:42 -0400

    script.js updates

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
 })
 
-// current day at the top of the calendar
+//variable
+var saveBtn = $(".saveBtn");
+
+// displays current day at the top of the calendar
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
-//timeblocks and color changes
-function colorChange() {
-    
+// function to save to local storage
+function saveLocal(key, value) {
+    localStorage.setItem(key, value);
 }
+
+//function for persistence
+
+//timeblocks and color changes for past, present, and future
+function colorChange() {
+    var hour = moment().hours();
+
+
+}
+
+//function to upload data
+
+//on click listener to save info
+$(".saveBtn").on("click", function ());
+
+//function to push data from line 15
